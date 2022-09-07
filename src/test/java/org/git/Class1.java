3109f74085d562e14e7792f8f316ca5b71627fd9
abdulraheman123 <abdul17jan97@gmail.com>
2022-09-07 16:25:55 +0530

    test case2

--- src/test/java/org/git/Class1.java
+++ src/test/java/org/git/Class1.java
@@ -1,5 +1,12 @@
 package org.git;
 
 public class Class1 {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("test1");
+		System.out.println("test4");
+		System.out.println("test5");
+	}
 
 }
